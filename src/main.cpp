31bd588f47e2163ad2eb9324a3aeb9acfa7e613c
manu-adina <manu.adina-zada@hotmail.com>
2019-03-23 14:51:22 +0800

    Fixed UpdateError() order

--- src/main.cpp
+++ src/main.cpp
@@ -33,10 +33,9 @@ string hasData(string s) {
 int main() {
   uWS::Hub h;
 
+  // Change later.
   PID pid;
-  /**
-   * TODO: Initialize the pid variable.
-   */
+  pid.Init(0.15, 0.0, 2.5);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
@@ -54,8 +53,8 @@ int main() {
         if (event == "telemetry") {
           // j[1] is the data JSON object
           double cte = std::stod(j[1]["cte"].get<string>());
-          double speed = std::stod(j[1]["speed"].get<string>());
-          double angle = std::stod(j[1]["steering_angle"].get<string>());
+          //double speed = std::stod(j[1]["speed"].get<string>());
+          //double angle = std::stod(j[1]["steering_angle"].get<string>());
           double steer_value;
           /**
            * TODO: Calculate steering value here, remember the steering value is
@@ -63,6 +62,10 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
+
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
+
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
